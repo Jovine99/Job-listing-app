d6dacd287b2f7f1ba1bb40de7d24396a8dfecb83
Jovine Akinyi <jovineakinyi99@gmail.com>
2022-11-11 13:28:09 +0300

    initial commit

---
+++ app/Models/Listing.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+class Listing{
+    public static function all(){
+        return[
+            [
+                'id' => 1,
+                'title' =>'Listing One',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in tortor nunc. Nunc rutrum augue et lobortis scelerisque. Phasellus pulvinar ex rhoncus nunc varius varius. Integer vestibulum rutrum ligula elementum tempor.'
+            ],
+            [
+                'id' => 2,
+                'title' =>'Listing Two',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in tortor nunc. Nunc rutrum augue et lobortis scelerisque. Phasellus pulvinar ex rhoncus nunc varius varius. Integer vestibulum rutrum ligula elementum tempor.'
+            ]
+            ];
+    }
+    public static function find($id){
+     $listings = self::all();
+     foreach($listings as $listing){
+        if($listing['id'] ==$id){
+            return $listing;
+        }
+     }   
+    }
+}
